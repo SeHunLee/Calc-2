43ecb4d14e4a21f97b1e7313a8dd20f7c7836357
USER <USER@COM141-143>
2018-02-09 11:10:23 +0900

    수정

--- Calc/src/SubClass.java
+++ Calc/src/SubClass.java
@@ -5,6 +5,7 @@ public class SubClass implements Hab {
 		System.out.println("»©±â ¿¬»ê");
 		int num1 = nc.getNum1();
 		int num2 = nc.getNum2();
+		System.out.println();
 		
 		System.out.println(num1+"-"+num2+"="+(num1-num2));
 	}
